e7f7ede0f8ebf16769613eeeb4f6f10d4761d035
amirreza madandar <amir.front.dev@gmail.com>
2023-08-19 18:41:07 +0330

    adding loading to the ptoject

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import Home from "./Pages";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./Component Scoroll/Scroll";
+import Home from "./Pages";
 import Shop from "./Pages/shop";
 import Cart from "./Pages/cart";
 import Blog from "./Pages/blog";
 import About from "./Pages/about";
 import Contact from "./Pages/contact";
-import ScrollToTop from "./Component Scoroll/Scroll";
 import Page404 from "./Pages/notFound";
-
+import "./App.css";
+import "./loading.css";
+import loading from "./images/15.gif";
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.onload = () => {
+      setIsLoading(false);
+    };
   }, []);
 
   return (
-    <>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Page404 />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div className="page-content">
+      {isLoading ? (
+        <div className="loading">
+          <img src={loading} alt="Loading..." />
+        </div>
+      ) : (
+        <>
+          <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </BrowserRouter>
+        </>
+      )}
+    </div>
   );
 };
 
